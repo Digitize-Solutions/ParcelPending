cd95880c87f70be98c58b7779f96eec49d9bbbc8
theVineetTanwar <95026269+theVineetTanwar@users.noreply.github.com>
2023-01-05 10:10:57 +0530

    updated base url for APIs

--- textures/digitizeOakmasterUtils.js
+++ textures/digitizeOakmasterUtils.js
@@ -41,8 +41,8 @@ const garageSampleData = {
 
 
 // const BASE_API_PATH = 'http://localhost:3000';
-const BASE_API_PATH = 'https://postmark-server.herokuapp.com';
-// const BASE_API_PATH = 'https://oakmaster-proxy.3kit.com';
+// const BASE_API_PATH = 'https://postmark-server.herokuapp.com';
+const BASE_API_PATH = 'https://oakmaster-proxy.3kit.com';
 const TRUSS_PDF_API_PATH = '/api/pdf/truss';
 const GARAGE_PDF_API_PATH = '/api/pdf/garage';
 
